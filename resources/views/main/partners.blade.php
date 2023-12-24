3399e258e71a63bfeb3f906541cd0ab79159cb02
Arturio186 <bain.romanoff@mail.ru>
2023-12-24 21:18:48 +0500

    Partners page

--- resources/views/main/partners.blade.php
+++ resources/views/main/partners.blade.php
@@ -8,6 +8,28 @@
 @section('content')
 <div class="partners-container">
         <h1>Наши Партнёры</h1>
-        
+        <div class="partners">
+            <a href="https://www.google.com" target="_blank">
+                <img src="./images/partner_logo3.svg" alt="Партнёр">
+            </a>
+            <a href="https://www.google.com" target="_blank">
+                <img src="./images/partner_logo2.svg" alt="Партнёр">
+            </a>
+            <a href="https://www.google.com" target="_blank">
+                <img src="./images/partner_logo1.svg" alt="Партнёр">
+            </a>
+            <a href="https://www.google.com" target="_blank">
+                <img src="./images/partner_logo2.svg" alt="Партнёр">
+            </a>
+            <a href="https://www.google.com" target="_blank">
+                <img src="./images/partner_logo3.svg" alt="Партнёр">
+            </a>
+            <a href="https://www.google.com" target="_blank">
+                <img src="./images/partner_logo1.svg" alt="Партнёр">
+            </a>
+            <a href="https://www.google.com" target="_blank">
+                <img src="./images/partner_logo2.svg" alt="Партнёр">
+            </a>
+        </div>
     </div>
 @endsection
