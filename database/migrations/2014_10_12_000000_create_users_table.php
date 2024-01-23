309e62976497b6b571f5a59a4ac266208298a43f
Michael Gyndybin <misamaikl97@gmail.com>
2024-01-23 20:14:25 +0500

    j

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('surname');
             $table->string('name');
+            $table->string('mid_name');
             $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('work_space');
             $table->integer('role')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
