4bfc05bd897a75bf80a2248387e904361d01267f
Arturio186 <bain.romanoff@mail.ru>
2024-01-23 23:33:01 +0500

    Fix Merge Conflict

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
-            $table->string('surname');
-            $table->string('name');
-            $table->string('mid_name');
-            $table->string('email')->unique();
-            $table->string('phone_number');
-            $table->string('work_space');
-=======
             $table->string('surname')->nullable();
             $table->string('name');
             $table->string('mid_name')->nullable();
@@ -30,7 +22,6 @@ return new class extends Migration
             $table->string('work_space')->nullable();
             $table->string('study_place')->nullable();
             $table->string('photo_path')->nullable();
->>>>>>> misa
             $table->integer('role')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
