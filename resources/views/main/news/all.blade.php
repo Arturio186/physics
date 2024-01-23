c61592b7b3f518439f528434e3005ecf083f33f6
Arturio186 <bain.romanoff@mail.ru>
2024-01-23 19:39:36 +0500

    Test commit

--- resources/views/main/news/all.blade.php
+++ resources/views/main/news/all.blade.php
@@ -8,39 +8,43 @@
 
 @section('content')
     <h1 class="title">Новости</h1>
-    <div class="news-list">
-        @foreach($news as $item)
-            <div class="news-card">
-                <img class="news-image" src="https://via.placeholder.com/400x400" alt="News Image">
-                <div class="news-content">
-                    <h2 class="news-title">{{$item->title}}</h2>
-                    <p class="news-date">Дата: {{$item->date}}</p>
-                    <p class="news-description">{{$item->short_description}}</p>
-                    <a class="news-link" href="{{route('news.show', $item->id)}}">Подробнее</a>
+    @if (count($news) !== 0)
+        <div class="news-list">
+            @foreach($news as $item)
+                <div class="news-card">
+                    <img class="news-image" src="https://via.placeholder.com/400x400" alt="News Image">
+                    <div class="news-content">
+                        <h2 class="news-title">{{$item->title}}</h2>
+                        <p class="news-date">Дата: {{$item->date}}</p>
+                        <p class="news-description">{{$item->short_description}}</p>
+                        <a class="news-link" href="{{route('news.show', $item->id)}}">Подробнее</a>
+                    </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
-    <div class="pagination-container">
-        <ul class="pagination">
-            @if ($news->onFirstPage())
-                <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
-            @else
-                <li class="page-item"><a class="page-link" href="{{ $news->previousPageUrl() }}">&laquo;</a></li>
-            @endif
-
-            @foreach ($news->getUrlRange(1, $news->lastPage()) as $page => $url)
-                <li class="page-item {{ $page == $news->currentPage() ? 'active' : '' }}">
-                    <a class="page-link" href="{{ $url }}">{{ $page }}</a>
-                </li>
             @endforeach
+        </div>
+        <div class="pagination-container">
+            <ul class="pagination">
+                @if ($news->onFirstPage())
+                    <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
+                @else
+                    <li class="page-item"><a class="page-link" href="{{ $news->previousPageUrl() }}">&laquo;</a></li>
+                @endif
 
-            @if ($news->hasMorePages())
-                <li class="page-item"><a class="page-link" href="{{ $news->nextPageUrl() }}">&raquo;</a></li>
-            @else
-                <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
-            @endif
-        </ul>
-    </div>
+                @foreach ($news->getUrlRange(1, $news->lastPage()) as $page => $url)
+                    <li class="page-item {{ $page == $news->currentPage() ? 'active' : '' }}">
+                        <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                    </li>
+                @endforeach
+
+                @if ($news->hasMorePages())
+                    <li class="page-item"><a class="page-link" href="{{ $news->nextPageUrl() }}">&raquo;</a></li>
+                @else
+                    <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
+                @endif
+            </ul>
+        </div>
+    @else
+        <p class="message">Новостей нет</p>
+    @endif
 
 @endsection
