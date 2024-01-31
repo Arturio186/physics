7935f2fc1165f8bec0040301876315b6ab3dd4da
Arturio186 <bain.romanoff@mail.ru>
2024-01-31 16:11:18 +0500

    del dd from managers controller

--- app/Http/Controllers/ManagementController.php
+++ app/Http/Controllers/ManagementController.php
@@ -10,7 +10,7 @@ class ManagementController extends Controller
     public function index() 
     {
         $managers = Manager::all();
-        dd($managers);
+        
         return view('main.management', compact('managers'));
     }
 }
